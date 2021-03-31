3001c704fedabec3c597074f6f8475b35226be4f
Rikicb9 <rcastelletb@gmail.com>
2021-03-31 19:09:28 +0200

    Add message box asking user name and then change header

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// Image switcher code
+
 let myImage = document.querySelector("img");
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute("src");
@@ -6,4 +8,30 @@ myImage.onclick = function() {
     } else {
         myImage.setAttribute("src", "images/pintura-negra-2.jpg");
     }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Saturno te está devorando, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Saturno te está devorando, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
